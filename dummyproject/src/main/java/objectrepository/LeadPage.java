9cc0c42ad96412d1dcc42dca251297decb79fb5f
hp <hp@DESKTOP-7KVO2NO>
2021-03-14 20:06:08 -1200

    added lead and login page

---
+++ dummyproject/src/main/java/objectrepository/LeadPage.java
@@ -0,0 +1,15 @@
+package objectrepository;
+
+import org.testng.annotations.Test;
+/*
+ * @poonam
+ */
+public class LeadPage {
+	@Test 
+	
+	public void createlead() {
+		System.out.print("creating lead");
+		
+	}
+
+}
